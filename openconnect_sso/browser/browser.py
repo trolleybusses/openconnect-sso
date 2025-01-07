690677a18d7e4842c468cf7e47658e80a085fa8e
Matt Pearce <mpearce@roku.com>
2025-01-07 12:40:03 +0000

    Fix chrome webdriver

--- openconnect_sso/browser/browser.py
+++ openconnect_sso/browser/browser.py
@@ -17,10 +17,10 @@ from selenium.common.exceptions import TimeoutException
 # from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
-# from webdriver_manager.core.os_manager import ChromeType
+# from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.firefox.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.log import Log
 from ..config import DisplayMode
